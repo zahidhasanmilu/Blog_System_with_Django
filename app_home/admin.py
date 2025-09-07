e9423d35630310b8759309407eff35fbd83f9486
zahidhasanmilu <minions.milu889@gmail.com>
2025-09-07 16:13:36 +0600

    blog, category, tags, blogimages models

--- app_home/admin.py
+++ app_home/admin.py
@@ -1,3 +1,36 @@
 from django.contrib import admin
+from .models import Category, Tag, Blog, BlogImage
 
-# Register your models here.
+# ✅ Inline view for Blog Images
+class ImageInline(admin.StackedInline):
+    """Stacked Inline View for BlogImage"""
+    model = BlogImage
+    min_num = 1
+    extra = 0  # অতিরিক্ত খালি ফিল্ড দেখাবে না
+
+# ✅ Admin view for Blog
+@admin.register(Blog)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'category', 'get_tags_display', 'created_date']
+    list_filter = ['category', 'tags', 'created_date']
+    search_fields = ['title', 'content', 'author__email']
+    ordering = ['-created_date']
+
+    inlines = [ImageInline]
+
+    def get_tags_display(self, obj):
+        return ', '.join(tag.title for tag in obj.tags.all())
+    get_tags_display.short_description = 'Tags'
+
+# ✅ Register other models
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug']
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug']
+
+@admin.register(BlogImage)
+class BlogImageAdmin(admin.ModelAdmin):
+    list_display = ['blog', 'image']
--- app_home/models.py
+++ app_home/models.py
@@ -1,3 +1,88 @@
+import uuid
 from django.db import models
+from django.utils.text import slugify
+from django.conf import settings
+import os
 
-# Create your models here.
+def sanitize_filename(filename):
+    forbidden = '<>:"/\\|?*'
+    trans = str.maketrans({c: '_' for c in forbidden})
+    return filename.translate(trans)
+
+# ✅ Generate unique slug
+def generate_unique_slug(model, base_slug):
+    unique_slug = f"{base_slug}-{uuid.uuid4().hex[:6]}"
+    while model.objects.filter(slug=unique_slug).exists():
+        unique_slug = f"{base_slug}-{uuid.uuid4().hex[:6]}"
+    return unique_slug
+# ---------
+
+class Category(models.Model):
+    title = models.CharField(max_length=50,unique=True)
+    slug = models.SlugField(unique=True, blank=True, null=True, max_length=300)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50,unique=True)
+    slug = models.SlugField(unique=True, blank=True, null=True, max_length=300)
+
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+class Blog(models.Model):
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,  # <-- এখানে fix
+        related_name='user_blogs',
+        on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(unique=True, blank=True, null=True, max_length=300)
+    content = models.TextField()
+    category = models.ForeignKey(Category, related_name='category_blogs', on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag, related_name='tag_blogs')
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        base_slug = slugify(self.title)
+        if not self.slug:
+            self.slug = generate_unique_slug(Blog, base_slug)
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        ordering = ['-created_date']
+
+
+
+class BlogImage(models.Model):
+    blog = models.ForeignKey(Blog, related_name='blog_images', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='blog_images/')
+
+    def save(self, *args, **kwargs):
+        if self.image:
+            base, ext = os.path.splitext(self.image.name)
+            filename = sanitize_filename(base)
+            self.image.name = f"{filename}_{uuid.uuid4().hex[:6]}{ext}"
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.blog.title
